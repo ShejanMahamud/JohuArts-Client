f59ad854a93fdf6fffedff84206654cdbee4a4ae
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-04-26 04:34:36 +0600

    Login Register page design complete

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -4,6 +4,8 @@ import {
 } from "react-router-dom";
 import Root from '../layouts/Root';
 import ErrorPage from '../pages/ErrorPage';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 import Home from './../pages/Home';
 
 const Route = createBrowserRouter([
@@ -14,6 +16,14 @@ const Route = createBrowserRouter([
       {
         path: '/',
         element: <Home/>
+      },
+      {
+        path: '/login',
+        element: <Login/>
+      },
+      {
+        path: '/register',
+        element: <Register/>
       }
     ],
     errorElement: <ErrorPage/>
